99bbcfc505cf8a53f2a1dd988989843a002d8cf0
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-29 19:47:17 +0700

    update GET method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import { createServer, IncomingMessage } from "node:http";
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import type { User } from "./models/User.js";
 
 const hostName = "192.168.20.142";
 const port = 9999;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Welcome to my http server");
-});
+const server = createServer(
+  async (req: IncomingMessage, res: ServerResponse) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST, GET, OPTIONS, PUT, PATCH, DELETE",
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    switch (`${req.method} ${req.url}`) {
+      case "GET /":
+        res.writeHead(200);
+        res.end("Welcome to my http server!");
+        break;
+      default:
+        res.writeHead(405, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Method not allowed" }));
+        break;
+    }
+  },
+);
 
 server.listen(port, hostName, () => {
   console.log(`Server is running at http://${hostName}:${port}`);
