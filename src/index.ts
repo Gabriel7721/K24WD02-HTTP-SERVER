cdb4172493f70b7583b6906b48de350a1d108b0a
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-29 19:33:37 +0700

    Helper function: JSON --> Javascript Object (users)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { createServer } from "node:http";
+import { createServer, IncomingMessage } from "node:http";
+import type { User } from "./models/User.js";
 
 const hostName = "192.168.20.142";
 const port = 9999;
@@ -12,3 +13,28 @@ const server = createServer((req, res) => {
 server.listen(port, hostName, () => {
   console.log(`Server is running at http://${hostName}:${port}`);
 });
+
+// NOTE: Data structures
+let nextId: number = 1;
+const users: User[] = [
+  { id: nextId++, name: "Harry Potter", email: "hp@gmail.com" },
+  { id: nextId++, name: "Voldermolt", email: "v@gmail.com" },
+];
+
+// NOTE: Helper function: JSON --> Javascript Object (users)
+function parseBody(req: IncomingMessage): Promise<any> {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (c) => {
+      body += c;
+    });
+
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
